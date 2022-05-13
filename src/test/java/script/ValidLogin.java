bc626f64db1496bc23c6e443ab92489e885b3ccc
HP <HP@NamiChandra>
2022-05-13 07:51:22 +0530

    adding invalid login test case

--- src/test/java/script/ValidLogin.java
+++ src/test/java/script/ValidLogin.java
@@ -9,16 +9,16 @@ import page.LoginPage;
 
 public class ValidLogin extends BaseTest{
 
-	@Test(priority = 1,groups = {"smoke"})
-	public void testValidLogin() {
+	@Test(dataProvider = "getData", priority = 1,groups = {"smoke"})
+	public void testValidLogin(String un,String pw,String eTitle) {
 //		1.	Enter valid user name (admin)
 		test.info("	Enter valid user name (admin)");
 		LoginPage loginPage=new LoginPage(driver);  
-		loginPage.setUserName("admin");
+		loginPage.setUserName(un);
 		
 //		2.	Enter valid password (manager)
 		test.info("Enter valid password (manager)");
-		loginPage.setPassword("damager");
+		loginPage.setPassword(pw);
 		
 //		3.	Click on login button
 		test.info("Click on login button");
@@ -27,10 +27,9 @@ public class ValidLogin extends BaseTest{
 //		4.	Verify that home page is displayed
 		test.info("Verify that home page is displayed");
 		EnterTimeTrackPage ettPage=new EnterTimeTrackPage(driver);
-		boolean result = ettPage.verifyPageTitle(wait, "actiTIME - Enter Time-Track");
+		boolean result = ettPage.verifyPageTitle(wait, eTitle);
 		Assert.assertTrue(result, "Home page is not displayed");
 		test.pass("home page is displayed");
-		//true--title is matching- home page is displayed--PASS
-		//false-title is not matching --home page is not displayed-FAIL
+
 	}
 }
